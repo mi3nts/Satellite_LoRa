b325d38c166fd65bdea71af8cb29cfb9f0f8a33b
prabudewage <pxh180012@utdallas.edu>
2022-01-17 16:17:53 -0600

    6th update

--- firmware/GOES_16_data_download.py
+++ firmware/GOES_16_data_download.py
@@ -1,8 +1,18 @@
-import s3fs
+
+import xarray as xr
+import matplotlib.pyplot as plt
+from pyproj import Proj
+import numpy as np
+import cartopy
+
+# s3Fs is a Pythonic file interface to S3 (Amazon Simple Storage Service (Amazon S3))
+import s3fs   
 import os
 
-fs = s3fs.S3FileSystem(anon=True)
+# The top-level class S3FileSystem holds connection information and allows typical -
+# -file-system style operations like cp, mv, ls, du, glob, etc., as well as put/get of local files to/from S3.
+fs = s3fs.S3FileSystem(anon=True)       
 files= fs.glob('noaa-goes16/ABI-L2-AODC/2021/140/01/*')
-
-print(files)
+# print(files)
 fs.get(files[1], "/home/prabu/Desktop/Satellite Data/")
+ds_GOES = xr.open_dataset("/home/prabu/Research/GOES_16_data/OR_ABI-L2-AODC-M6_G16_s20211400106152_e20211400108525_c20211400110132.nc")
